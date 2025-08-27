6bab03a06b76e9cf572ff74c055ddf4283f7fe39
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2025-08-26 21:42:10 -0400

    Updated rss file.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,8 +4,8 @@ import { getCollection } from "astro:content";
 export async function get() {
   const posts = await getCollection('posts');
   return rss({
-    title: 'Astro Learner | Blog',
-    description: 'My journey learning Astro',
+    title: 'Lifye',
+    description: 'My journey learning Astro and learning how to write a blog.',
     site: 'https://my-blog-site.netlify.app',
     items: posts.map((post) => ({
       title: post.data.title,
